c19d6fa48dd1148c1418afbf6f616d9c091aeb26
Matt Beckett <beck24@gmail.com>
2012-10-25 16:09:23 -0600

    makes avatar link an action button

--- views/default/draw/avataredit.php
+++ views/default/draw/avataredit.php
@@ -5,7 +5,8 @@ $page_owner = elgg_get_page_owner_entity();
 echo elgg_view('output/url', array(
     'href' => 'draw/avatar/' . $page_owner->username,
     'text' => elgg_echo('draw:avatar'),
-    'is_trusted' => true
+    'is_trusted' => true,
+    'class' => 'elgg-button elgg-button-action'
 ));
 
 echo '<br><br>';
